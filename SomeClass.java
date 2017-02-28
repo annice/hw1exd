eef4e0475a7a9003d3f239f1b58ba90bf86b1100
Annice Mathew <annsays1@gmail.com>
2017-02-28 09:00:53 -0800

    Refactored fnOne

--- SomeClass.java
+++ SomeClass.java
@@ -1,10 +1,10 @@
 public class SomeClass {
-	public void fnOne() {
+	public void thoughtForTheDay() {
 	
 			System.out.println("I couldn't repair your brakes so I made your horn louder");
 	}
 	public static void main(String[] args) {
 		SomeClass sm = new SomeClass();
-		sm.fnOne();
+		sm.thoughtForTheDay();
 	}
 }
