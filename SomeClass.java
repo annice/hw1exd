02f88fb852e03b0b699d016ceefc61c9991a5616
Annice Mathew <annsays1@gmail.com>
2017-02-28 09:04:49 -0800

    Added events

--- SomeClass.java
+++ SomeClass.java
@@ -3,8 +3,13 @@ public class SomeClass {
 	
 			System.out.println("I couldn't repair your brakes so I made your horn louder");
 	}
+		public void upcomingEvents() {
+	
+			System.out.println("Party at Jane's house today");
+	}
 	public static void main(String[] args) {
 		SomeClass sm = new SomeClass();
 		sm.thoughtForTheDay();
+		sm.upcomingEvents();
 	}
 }
