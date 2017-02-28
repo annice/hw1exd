9cc3eba6db8b724bec1c928604bef85cb0da468c
Annice Mathew <annsays1@gmail.com>
2017-02-28 09:00:17 -0800

    Initial

---
+++ SomeClass.java
@@ -0,0 +1,10 @@
+public class SomeClass {
+	public void fnOne() {
+	
+			System.out.println("I couldn't repair your brakes so I made your horn louder");
+	}
+	public static void main(String[] args) {
+		SomeClass sm = new SomeClass();
+		sm.fnOne();
+	}
+}
